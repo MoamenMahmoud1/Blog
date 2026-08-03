ad21c905347b36e4062e0654f62854c0ef6d302f
Joe <joamamoud37@gmail.com>
2026-08-03 10:43:26 -0400

    Fix HTTPS redirect in test settings

--- src/mysite/test_settings.py
+++ src/mysite/test_settings.py
@@ -1,5 +1,7 @@
 from .settings import *
 
+SECURE_SSL_REDIRECT = False
+
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
@@ -10,6 +12,7 @@ CACHES = {
 EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
 CELERY_TASK_ALWAYS_EAGER = True
 CELERY_TASK_EAGER_PROPAGATES = True
+
 PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
 STORAGES["staticfiles"] = {
     "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
